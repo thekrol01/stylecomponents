f752d3f21c5a54040f68dd9f41deb39e7625e7aa
unknown <edvdvlies@gmail.com>
2018-12-05 12:40:51 +0100

    styling confirm

--- common/Confirm.js
+++ common/Confirm.js
@@ -4,42 +4,58 @@ import { CardSection } from './CardSection';
 import { Button } from './Button';
 
 const Confirm = ({ children, visible, onAccept, onDecline }) => {
-  const { containerStyle, textStyle, cardSectionStyle } = styles;
+  const { containerStyle, textStyle, cardSectionStyle, buttonSectionStyle } = styles;
 return (
   <Modal
-  visible={visible}
-  transparent
-  animationType="slide"
-  onRequestClose={() => {}}
-  >
-  <View style={containerStyle}>
-  <CardSection style={cardSectionStyle} >
-    <Text style={textStyle}>{children}</Text>
-  </CardSection>
-  <CardSection>
-  <Button onPress={onAccept}>Yes</Button>
-  <Button onPress={onDecline}>No</Button>
-  </CardSection>
-  </View>
+    visible={visible}
+    transparent
+    animationType="slide"
+    onRequestClose={() => {}}
+    >
+    <View style={containerStyle}>
+      <CardSection style={cardSectionStyle} >
+        <Text style={textStyle}>{children}Meldingstekst</Text>
+      </CardSection>
+      <CardSection style={buttonSectionStyle}>
+        <Button onPress={onAccept}>Yes</Button>
+        <Button onPress={onDecline}>No</Button>
+      </CardSection>
+    </View>
   </Modal>
 );
 };
 const styles = {
+  containerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+  },
   cardSectionStyle: {
-    justifyContent: 'center'
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '90%',
+    height: 200,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  buttonSectionStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 20,
+    width: '90%',
+    height: 85,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
   },
   textStyle: {
     flex: 1,
-    fontSize: 18,
     textAlign: 'center',
-    lineHeight: 40
+    fontSize: 16,
+    lineHeight: 20,
   },
-  containerStyle: {
-    backgroundColor: 'rgba(0, 0, 0, 0.75)',
-    position: 'relative',
-    flex: 1,
-    justifyContent: 'center'
-  }
+
 };
 
 export { Confirm };
