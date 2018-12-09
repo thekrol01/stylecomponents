e062546078573634525f5651fa2fe194005d245f
unknown <edvdvlies@gmail.com>
2018-12-09 11:01:38 +0100

    add splash img

--- common/Header.js
+++ common/Header.js
@@ -1,33 +1,30 @@
 // import libraries for making a registerComponent
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, Text  } from 'react-native';
 
 const Header = (props) => {
   const { textStyle, viewStyle } = styles;
   return (
     <View style={viewStyle}>
-<Text style={textStyle}>{props.headerText}</Text>
-</View>
+      <Text style={textStyle}>{props.headerText}HeaderText</Text>
+    </View>
 );
 };
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8',
+    flex: 1,
+    paddingTop: (Platform.OS == 'ios') ? 20 : 0,
+    backgroundColor: '#f5bd00',
     justifyContent: 'center',
     alignItems: 'center',
-    height: 60,
-    paddingTop: 15,
-    shadowcolor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
     elevation: 2,
     position: 'relative'
   },
   textStyle: {
-    fontSize: 20
+    fontSize: 20,
+    color: '#ffffff',
   }
-
 };
 
 export { Header };
--- common/Input.js
+++ common/Input.js
@@ -5,40 +5,47 @@ const Input = ({ label, value, onChangeText, placeHolder, secureTextEntry, edita
   const { inputStyle, containerStyle, labelStyle } = styles;
   return (
   <View style={containerStyle}>
-  <Text style={labelStyle}>{label}</Text>
-  <TextInput
-  secureTextEntry={secureTextEntry}
-  placeholder={placeHolder}
-  autoCorrect={false}
-  style={inputStyle}
-  value={value}
-  onChangeText={onChangeText}
-  editable={editable}
-  />
+    <Text style={labelStyle}>{label}Label:</Text>
+    <TextInput
+      style={inputStyle}
+      secureTextEntry={secureTextEntry}
+      placeholder={placeHolder}
+      autoCorrect={false}
+      value={value}
+      onChangeText={onChangeText}
+      editable={editable}
+      placeholder='placeholder tekst'
+      />
   </View>
 );
 };
 
 const styles = {
-  inputStyle: {
-    color: '#000',
-    paddingRight: 5,
-    paddingLeft: 5,
-    fontSize: 18,
-    lineHeight: 23,
-    flex: 2
+  containerStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 40,
+    paddingRight: 20,
   },
   labelStyle: {
-    fontSize: 18,
+    fontSize: 14,
     paddingLeft: 20,
     flex: 1
   },
-  containerStyle: {
+  inputStyle: {
+    color: '#000',
+    fontSize: 14,
+    lineHeight: 23,
+    flex: 3,
+    width: '100%',
     height: 40,
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center'
-  }
+    borderColor: '#cccccc',
+    borderWidth: .5,
+    backgroundColor: '#fff',
+    textAlign:'left',
+    paddingLeft: 10,
+  },
 };
 
 export { Input };
